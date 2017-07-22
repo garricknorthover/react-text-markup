151f7d33758a11814613332a0cb2da85fe990d42
garricknorthover <garricknorthover@gmail.com>
2017-07-22 14:15:03 +1200

    first stab

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class TypeInText extends Component {
+
+  state = {value: ''}
+
+  handleChange = (e) => {
+    this.setState({value: e.target.value})
+  }
+  render() {
+
+    const {value} = this.state
+
+
+    return(
+      <div>
+      <div>
+        <textarea value={value} onChange={this.handleChange} />
+
+        </div>
+
+        <div className="display-linebreak">
+          {value}
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <TypeInText />
+      </div>
+    );
+  }
+}
+
+export default App;
