ff535c46efa8f14acdaacf17a58857755c05d1d1
garricknorthover <garricknorthover@gmail.com>
2017-07-25 16:43:17 +1200

    dangerously set html

--- src/App.js
+++ src/App.js
@@ -1,56 +1,66 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
-
 class TypeInText extends Component {
 
-  state = {value: ''}
+  state = {
+    value: ''
+  }
 
   handleChange = (e) => {
     this.setState({value: e.target.value})
   }
+
   render() {
 
     const {value} = this.state
 
-
-    return(
+    return (
       <div>
-      <div>
-        <textarea value={value} onChange={this.handleChange} />
-
-        </div>
-
-        <div className="display-linebreak">
-          {value}
-        </div>
+        
+          <textarea value={value} onChange={this.handleChange}/>
+          <div className="display-linebreak col">
+          <MarkIt value={value}/>
+        </div >
       </div>
     )
   }
 }
 
+class MarkIt extends Component {
 
+  render() {
 
+    const outValue = this
+      .props
+      .value
+      .replace(/#(.\S+)#/ig, '<b>$1</b>')
 
+    const outValueHTML = {__html: outValue} 
 
+    return (
+        <div dangerouslySetInnerHTML={outValueHTML} />     
+    )
+  }
 
-
-
-
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
-        <TypeInText />
+        <TypeInText/>
       </div>
     );
   }
